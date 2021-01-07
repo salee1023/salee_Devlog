30be33efdc3df3c4e69b2e911806ce0131a7336f
salee1023 <dltmddk1023@gmail.com>
2021-01-07 16:51:33 +0900

    setting

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This blog built with [Gastby starter bee](https://github.com/JaeYeopHan/gatsby-starter-bee) 🐝
 
+I referenced this [blog](https://delivan.dev/web/start-gatsby-blog/) 
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  title: `Bee starter`,
-  description: `Blog posted about ...`,
-  author: `[Your Name]`,
-  introduction: `I explain with words and code. I explain with words and code. I explain with words and code.`,
+  title: `salee_Devlog`,
+  description: `Back To Basic!`,
+  author: `[salee]`,
+  introduction: `Hello👋`,
   siteUrl: `https://gatsby-starter-bee.netlify.com`, // Your blog site url
   social: {
     twitter: ``, // Your Twitter account
-    github: ``, // Your GitHub account
+    github: `salee102310`, // Your GitHub account
     medium: ``, // Your Medium account
     facebook: ``, // Your Facebook account
     linkedin: ``, // Your LinkedIn account
   },
-  icon: `content/assets/felog.png`, // Add your favicon
+  icon: ``, // Add your favicon
   keywords: [`blog`],
   comment: {
     disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    utterances: '', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: '',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
